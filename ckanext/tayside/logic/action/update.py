ddc136abec84c1be03ecf6adb22374b8cbd137ed
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-08-17 11:47:24 +0200

    Make hero image on homepage configurable

---
+++ ckanext/tayside/logic/action/update.py
@@ -0,0 +1,11 @@
+from ckan.logic.action import update as update_core
+import ckan.lib.uploader as uploader
+
+
+def config_option_update(context, data_dict):
+    upload = uploader.get_uploader('admin')
+    upload.update_data_dict(data_dict, 'hero_image_url', 'hero_image_upload',
+                            'clear_hero_image_upload')
+    upload.upload(uploader.get_max_image_size())
+
+    return update_core.config_option_update(context, data_dict)
