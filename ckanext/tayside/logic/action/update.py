e61bd9a56b31dde461ad0cb82e3140bd0dbfa958
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-08-21 13:52:26 +0200

    Fix bug for saving images in config

--- ckanext/tayside/logic/action/update.py
+++ ckanext/tayside/logic/action/update.py
@@ -4,8 +4,12 @@ import ckan.lib.uploader as uploader
 
 def config_option_update(context, data_dict):
     upload = uploader.get_uploader('admin')
+
     upload.update_data_dict(data_dict, 'hero_image_url', 'hero_image_upload',
                             'clear_hero_image_upload')
+
+    upload.upload(uploader.get_max_image_size())
+
     upload.update_data_dict(data_dict, 'site_symbol_url', 'site_symbol_upload',
                             'clear_site_symbol_upload')
     upload.upload(uploader.get_max_image_size())
