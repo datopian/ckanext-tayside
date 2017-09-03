2060d54c012c05e6413fa578ae4b7cd9f76313dc
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-09-03 18:27:00 +0200

    Show/hide info block for allowed users

--- ckanext/tayside/fanstatic/js/lib/visibility-field.js
+++ ckanext/tayside/fanstatic/js/lib/visibility-field.js
@@ -4,16 +4,20 @@
 
   var visibilitySelect = document.querySelector('select[name="private"]')
   var allowedUsersField = document.querySelector('label[for="allowed_users"]')
+  var allowedUsersInfoBlock = document.querySelector('.allowed-users-info-block')
 
   if (visibilitySelect.value === 'False') {
     allowedUsersField.parentElement.style.display = 'none';
+    allowedUsersInfoBlock.style.display = 'none';
   }
 
   visibilitySelect.addEventListener('change', function() {
     if (visibilitySelect.value === 'False') {
       allowedUsersField.parentElement.style.display = 'none';
+      allowedUsersInfoBlock.style.display = 'none';
     } else if (visibilitySelect.value === 'True') {
       allowedUsersField.parentElement.style.display = 'block';
+      allowedUsersInfoBlock.style.display = 'block';
     }
   })
 
