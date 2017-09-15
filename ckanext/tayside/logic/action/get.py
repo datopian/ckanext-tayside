8dbc898cb4cbaa2ffda43cd852e21487402c2c7f
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-09-15 14:27:20 +0200

    Add additional DCAT fields

--- ckanext/tayside/logic/action/get.py
+++ ckanext/tayside/logic/action/get.py
@@ -1,5 +1,6 @@
 from ckan.logic.action import get as get_core
 from ckan.plugins import toolkit
+import ckan.lib.helpers as h
 
 
 @toolkit.side_effect_free
@@ -14,6 +15,7 @@ def package_show(context, data_dict):
     result = get_core.package_show(context, data_dict)
 
     dataset_id = result.get('id')
+    owner_org = result.get('owner_org')
     model = context.get('model')
     package = model.Package.get(dataset_id)
     groups = package.get_groups(group_type='group')
@@ -27,18 +29,62 @@ def package_show(context, data_dict):
 
     result = result.copy()
     extras = result.get('extras')
+    extra_theme_found = False
+    extra_publisher_email_found = False
+    extra_publisher_url_found = False
 
     if extras:
         for extra in extras:
             if extra.get('key') == 'theme':
                 extra['value'] = themes
-                return result
+                extra_theme_found = True
 
-        extras.append({'key': 'theme', 'value': themes})
-        # extras.append({'key': 'dcat_publisher_name', 'value': 'testirame'})
+            if extra.get('key') == 'publisher_email':
+                extra_publisher_email_found = True
+                data_dict = {'id': owner_org}
+                org = toolkit.get_action('organization_show')({}, data_dict)
+                organization_email = org.get('organization_email')
+                extra['value'] = organization_email
+
+            if extra.get('key') == 'publisher_url':
+                extra_publisher_url_found = True
+                publisher_url = h.url_for(controller='organization',
+                                          action='read',
+                                          id=owner_org, qualified=True)
+                extra['value'] = publisher_url
+
+        if not extra_theme_found:
+            extras.append({'key': 'theme', 'value': themes})
+
+        if not extra_publisher_email_found:
+            data_dict = {'id': owner_org}
+            org = toolkit.get_action('organization_show')({}, data_dict)
+            organization_email = org.get('organization_email')
+
+            if organization_email:
+                extras.append({
+                    'key': 'publisher_email',
+                    'value': organization_email
+                })
+
+        if not extra_publisher_url_found:
+            publisher_url = h.url_for(controller='organization', action='read',
+                                      id=owner_org, qualified=True)
+            extras.append({'key': 'publisher_url', 'value': publisher_url})
+
+        return result
     else:
         result.update({'extras': []})
         extras = result.get('extras')
-        # extras.append({'key': 'dcat_publisher_name', 'value': 'testirame'})
+
+    org = toolkit.get_action('organization_show')({}, {'id': owner_org})
+    organization_email = org.get('organization_email')
+
+    if organization_email:
+        extras.append({'key': 'publisher_email', 'value': organization_email})
+
+    publisher_url = h.url_for(controller='organization', action='read',
+                              id=owner_org, qualified=True)
+    extras.append({'key': 'publisher_url', 'value': publisher_url})
 
     return result
